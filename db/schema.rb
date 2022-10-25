06a29ad0a06097c71bed2add2f1e15c1251cecd2
bargholz eimii <818_77_@ezweb.ne.jp>
2022-10-25 16:05:15 +0900

    Add index

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 2022_10_25_043726) do
   enable_extension "plpgsql"
 
   create_table "labels", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
